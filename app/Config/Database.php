111268bfe10f1878915352c3b152821f9c56fe4a
gcordano <gustavohcordano@gmail.com>
2025-01-17 22:25:27 -0300

    teste

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => '',
-        'password'     => '',
-        'database'     => '',
+        'username'     => 'root',
+        'password'     => 'rootpassword',
+        'database'     => 'ci4tutorial',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
